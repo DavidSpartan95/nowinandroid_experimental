33979e6f5ab28f9cc2a01eb45a42ac3fa29e24b3
DavidSpartan95 <davidulwhan@hotmail.com>
2025-02-19 11:19:52 +0100

    UI: Stylized VideosScreen with ListItem

--- feature/video/src/main/java/com/google/samples/nowinandroid/videos/VideosScreen.kt
+++ feature/video/src/main/java/com/google/samples/nowinandroid/videos/VideosScreen.kt
@@ -16,37 +16,23 @@
 
 package com.google.samples.nowinandroid.videos
 
-import YouTubePlaylist
 import android.content.Intent
 import android.net.Uri
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import fetchYouTubePlayList
 import androidx.compose.foundation.lazy.items
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import coil.compose.AsyncImage
-import androidx.compose.ui.text.LinkAnnotation
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextLinkStyles
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.withLink
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.google.samples.apps.nowinandroid.core.designsystem.component.NiaLoadingWheel
@@ -54,16 +40,16 @@ import com.google.samples.apps.nowinandroid.feature.videos.R
 import com.google.samples.nowinandroid.videos.VideosViewModel.VideosUiState.Empty
 import com.google.samples.nowinandroid.videos.VideosViewModel.VideosUiState.Loading
 import com.google.samples.nowinandroid.videos.VideosViewModel.VideosUiState.Success
-
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
+import androidx.compose.material3.MaterialTheme
 
 @Composable
 fun VideosScreen(
     viewModel: VideosViewModel = hiltViewModel()
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-
     val context = LocalContext.current
-
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -75,46 +61,40 @@ fun VideosScreen(
                 contentDesc = stringResource(id = R.string.feature_video_loading),
             )
 
-            Empty -> Text(text = "No Videos")
+            Empty -> Text(text = "No Videos Found")
 
             is Success -> LazyColumn {
 
+                items((uiState as Success).playlists) { playList ->
 
-                items((uiState as Success).playlists) {playList ->
-                    Text(
-                        buildAnnotatedString {
-                            withLink(
-                                LinkAnnotation.Url(
-                                    "https://www.youtube.com/playlist?list=${playList.id}",
-                                    TextLinkStyles(style = SpanStyle(color = Color.Blue))
-                                )
-                            ) {
-                                append(playList.snippet.title)
-                            }
-                        }
-                    )
-
-                    Box(
+                    ListItem(
+                        leadingContent = {
+                            AsyncImage(
+                                model = playList.snippet.thumbnails.medium.url,
+                                contentDescription = null,
+                            )
+                        },
+                        headlineContent = { Text(playList.snippet.title) },
                         modifier = Modifier
                             .clickable {
                                 // Open the URL when the image is clicked
                                 val uri = "https://www.youtube.com/playlist?list=${playList.id}"
-                                // You can use your navigation or intent to open the link here
 
                                 val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
                                 context.startActivity(intent)
-                            }
-                    ) {
-                        AsyncImage(
-                            model = playList.snippet.thumbnails.high.url,
-                            contentDescription = null
-                        )
-                    }
+                            },
+                        overlineContent = { Text("Play List") },
+                        supportingContent = { Text(playList.snippet.description) },
+                        colors = ListItemDefaults.colors(
+                            containerColor = if (false) {
+                                MaterialTheme.colorScheme.surfaceVariant
+                            } else {
+                                Color.Transparent
+                            },
+                        ),
+                    )
                 }
             }
         }
-
-
-
     }
 }
